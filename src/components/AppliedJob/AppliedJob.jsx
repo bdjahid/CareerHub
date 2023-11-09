81778a9524e8655c1795aaadb4d430312804942b
Your Name <you@example.com>
2023-11-09 19:26:53 +0600

    filter

--- src/components/AppliedJob/AppliedJob.jsx
+++ src/components/AppliedJob/AppliedJob.jsx
@@ -4,25 +4,62 @@ import { getStoredJobApplication } from "../Utility/LocalStorage";
 
 
 const AppliedJob = () => {
-    const [appliedJob, setAppliedJob] = useState()
     const jobs = useLoaderData();
+    const [appliedJobs, setAppliedJobs] = useState([]);
+    const [displayJobs, setDisplayJobs] = useState([]);
+    const handleJobsFilter = filter => {
+        if (filter === "all") {
+            setDisplayJobs(appliedJobs)
+        }
+        else if (filter === 'remote') {
+            const remoteJobs = appliedJobs.filter(job => job.remote_or_onsite === 'Remote');
+            setDisplayJobs(remoteJobs)
+        }
+        else if (filter === 'onside') {
+            const onsideJobs = appliedJobs.filter(job => job.remote_or_onsite === 'Onsite');
+            setDisplayJobs(onsideJobs)
+        }
+    }
+    // console.log(job)
     useEffect(() => {
         const storedJobIds = getStoredJobApplication();
         if (jobs.length > 0) {
-            // const jobsApplied = jobs.filter(job => storedJobIds.includes(job.id));
-            const jobsApplied = [];
-            for (const id of storedJobIds) {
-                const job = jobs.find(job => job.ib === id);
-                if (job) {
-                    jobsApplied.push(job)
-                }
-            }
-            console.log(jobs, storedJobIds, jobsApplied)
+            const jobsApplied = jobs.filter(job => storedJobIds.includes(job.id));
+            // const jobsApplied = [];
+            // for (const id of storedJobIds) {
+            //     const job = jobs.find(job => job.id === id);
+            //     if (job) {
+            //         jobsApplied.push(job)
+            //     }
+            // }
+            setAppliedJobs(jobsApplied)
+            setAppliedJobs(jobsApplied)
+            // console.log(jobs, storedJobIds, jobsApplied)
         }
-    }, [])
+    }, [jobs])
     return (
         <div>
-            AppliedJob
+            <h2> AppliedJob:{appliedJobs.length}</h2>
+            <div>
+                <details className="dropdown">
+                    <summary className="m-1 btn">open or close</summary>
+                    <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+                        <li onClick={() => handleJobsFilter('all')}><a>All</a></li>
+                        <li onClick={() => handleJobsFilter('remote')}><a>Remote</a></li>
+                        <li onClick={() => handleJobsFilter('onside')}><a>Onside</a></li>
+                    </ul>
+                </details>
+            </div>
+            <ul>
+                {
+                    displayJobs.map(job => <li
+                        key={job.id}
+                    >
+
+                        <span>{job.job_title} {job.company_name}: {job.remote_or_onsite}</span>
+                    </li>)
+                }
+            </ul>
         </div>
     );
 };
